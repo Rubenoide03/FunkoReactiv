52d2d8ea522f47067bc42df8a1d2a2c69b561bf5
Rubenoide03 <rubenfernandezp03@gmail.com>
2023-10-10 03:14:27 +0200

    Added Query de Funko mas Caro

--- src/main/java/controllers/FunkoController.java
+++ src/main/java/controllers/FunkoController.java
@@ -1,4 +1,30 @@
 package controllers;
 
+import models.MyFunko;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 public class FunkoController {
+    private static FunkoController instance;
+    List<MyFunko> funkos;
+    private FunkoController(){
+
+    }
+    public static FunkoController getInstance(){
+        if(instance==null){
+            instance=new FunkoController();}
+        return instance;
+    }
+    public Optional<MyFunko> funkoMasCaro( ){
+        return funkos.stream().max(Comparator.comparingDouble(funko -> funko.precio()));
+
+
+    }
+
+
+
+
+
 }
